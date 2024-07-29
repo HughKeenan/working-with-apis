fb214e7fdaddb30112f7e4b9b434ed2bf0ea9a01
Hugh Keenan <hugh.keenan114@gmail.com>
2024-07-29 20:27:46 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "BI1725OF1TmY62Ijtqmpde_3GTc"
+const API_URL = "http://ci-jshint.herokuapp.com/api"
+const resultsmodal = new bootstrap.Modal(document.getElementById("resultsModal"))
